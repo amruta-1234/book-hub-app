957d4afa8ca6559c6a5310c61d605e4c562f4cce
amruta <amritashinde2018@gmail.com>
2023-04-07 13:25:44 +0000

    adding menu section in small device

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,32 +12,44 @@ const Header = props => {
   }
 
   return (
-    <nav className="nav-card">
-      <Link to="/">
-        <img
-          className="header-logo"
-          src="https://res.cloudinary.com/dpk1vp7a8/image/upload/v1679737640/Group_7731_1x_dcrcjk.png"
-          alt="website logo"
-        />
-      </Link>
+    <>
+      <nav className="nav-card">
+        <Link to="/">
+          <img
+            className="header-logo"
+            src="https://res.cloudinary.com/dpk1vp7a8/image/upload/v1679737640/Group_7731_1x_dcrcjk.png"
+            alt="website logo"
+          />
+        </Link>
 
-      <div className="header-card">
-        <ul className="link-item-card">
-          <Link className="link-item" to="/">
-            <li key="HOME">Home</li>
-          </Link>
-          <Link className="link-item" to="/shelf">
-            <li key="BOOKSHELVES">Bookshelves</li>
-          </Link>
-        </ul>
-        <button className="logout-btn" onClick={onClickLogout} type="button">
-          Logout
+        <div className="header-card">
+          <ul className="link-item-card">
+            <Link className="link-item" to="/">
+              <li key="HOME">Home</li>
+            </Link>
+            <Link className="link-item" to="/shelf">
+              <li key="BOOKSHELVES">Bookshelves</li>
+            </Link>
+          </ul>
+          <button className="logout-btn" onClick={onClickLogout} type="button">
+            Logout
+          </button>
+        </div>
+        <button className="menu-btn" type="button">
+          <HiMenu />
         </button>
+      </nav>
+      <div className="navbar-item-section-sm">
+        <Link className="link-item-sm" to="/">
+          <p className="home-item-sm">Home</p>
+        </Link>
+        <hr />
+        <Link className="link-item-sm" to="/shelf">
+          <p className="home-item-sm">Bookshelves</p>
+        </Link>
+        <hr />
       </div>
-      <button className="menu-btn" type="button">
-        <HiMenu />
-      </button>
-    </nav>
+    </>
   )
 }
 
